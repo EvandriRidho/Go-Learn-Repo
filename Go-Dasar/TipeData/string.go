501c790b8d70c81c7852523d29e319a3a817106a
unknown <evandriridho555@gmail.com>
2025-01-23 20:18:41 +0700

    Add desc

--- Go-Dasar/TipeData/string.go
+++ Go-Dasar/TipeData/string.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
+    // len untuk menghitung panjang string
     fmt.Println(len("Evandri"))
+    
+    // untuk mengambil karakter pada string dengan index
     fmt.Println("Evandri"[0])
     fmt.Println("Evandri"[6])
 }
